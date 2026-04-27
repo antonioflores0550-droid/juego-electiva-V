d4bf032d02652dc6941f8b879b1c504711137507
Antonio <antonioflores0550@gmail.com>
2026-04-27 14:29:39 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,112 +1,121 @@
+local Animador = require("animador")
+
+local sonidoActivo = true
+
 function love.load()
 
-    function safeLoad(archivo, tipo)
-        local status, asset = pcall(function()
-            if tipo == "image" then return love.graphics.newImage(archivo) end
-            if tipo == "audio" then return love.audio.newSource(archivo, "stream") end
-        end)
-        return status and asset or nil
-    end
+    imgFondo = love.graphics.newImage("fondo.png")
+    musica = love.audio.newSource("musica.mp3", "stream")
+    if musica then musica:setLooping(true); musica:play() end
 
-    imgFondo = safeLoad("fondo.png", "image")
-    imgP1    = safeLoad("minero1.jpg", "image")
-    imgP2    = safeLoad("minero2.png", "image")
-    imgOro   = safeLoad("oro.png", "image")
     
-    musicaFondo = safeLoad("musica.mp3", "audio")
-    if musicaFondo then
-        musicaFondo:setLooping(true)
-        musicaFondo:setVolume(0.5)
-        musicaFondo:play()
-    end
+    p1_anim = Animador.nueva(4, 2, 0.1)
+    p1_anim:agregarTira("minero1.png") 
+    p1_anim:agregarTira("IMG-20260426-WA0066(1).png")
 
-    p1 = { x = 100, y = 100, w = 45, h = 45, score = 0, speed = 300 }
-    p2 = { x = 650, y = 450, w = 45, h = 45, score = 0, speed = 300 }
-    
+    p2_anim = Animador.nueva(4, 2, 0.1)
+    p2_anim:agregarTira("minero2.png")
+    p2_anim:agregarTira("IMG-20260426-WA0064(1).png")
+
+    oro_anim = Animador.nueva(8, 1, 0.08)
+    oro_anim:agregarTira("oro.png")
+
+
+    p1 = { x = 100, y = 300, score = 0, speed = 300, tira = 1, w = 45, h = 45 }
+    p2 = { x = 650, y = 300, score = 0, speed = 300, tira = 1, w = 45, h = 45 }
     oro = { x = 400, y = 300, w = 32, h = 32 }
-    
     gameTimer = 60
     isGameOver = false
+
+    shaderTransparencia = love.graphics.newShader[[
+        vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
+            vec4 pixel = Texel(texture, texture_coords);
+            if ((pixel.r + pixel.g + pixel.b) < 0.1) { discard; }
+            return pixel * color;
+        }
+    ]]
 end
 
 function love.update(dt)
-    if isGameOver then return end
-
-    gameTimer = gameTimer - dt
-    if gameTimer <= 0 then isGameOver = true end
-
-    if love.keyboard.isDown("w") and p1.y > 0 then p1.y = p1.y - p1.speed * dt end
-    if love.keyboard.isDown("s") and p1.y < 555 then p1.y = p1.y + p1.speed * dt end
-    if love.keyboard.isDown("a") and p1.x > 0 then p1.x = p1.x - p1.speed * dt end
-    if love.keyboard.isDown("d") and p1.x < 755 then p1.x = p1.x + p1.speed * dt end
-
-    if love.keyboard.isDown("up") and p2.y > 0 then p2.y = p2.y - p2.speed * dt end
-    if love.keyboard.isDown("down") and p2.y < 555 then p2.y = p2.y + p2.speed * dt end
-    if love.keyboard.isDown("left") and p2.x > 0 then p2.x = p2.x - p2.speed * dt end
-    if love.keyboard.isDown("right") and p2.x < 755 then p2.x = p2.x + p2.speed * dt end
-
-    if checkCollision(p1, oro) then
-        p1.score = p1.score + 1
-        respawnOro()
-    elseif checkCollision(p2, oro) then
-        p2.score = p2.score + 1
-        respawnOro()
+    if gameTimer > 0 then
+        gameTimer = gameTimer - dt
+    else
+        gameTimer = 0
+        isGameOver = true
     end
+
+    local movP1, movP2 = false, false
+
+    if not isGameOver then
+
+        local limite_izq = 40
+        local limite_der = 720
+        local limite_sup = 180 
+        local limite_inf = 520
+
+
+        if love.keyboard.isDown("d") and p1.x < limite_der then p1.x = p1.x + p1.speed*dt; p1.tira = 1; movP1 = true end
+        if love.keyboard.isDown("a") and p1.x > limite_izq then p1.x = p1.x - p1.speed*dt; p1.tira = 2; movP1 = true end
+        if love.keyboard.isDown("w") and p1.y > limite_sup then p1.y = p1.y - p1.speed*dt; movP1 = true end
+        if love.keyboard.isDown("s") and p1.y < limite_inf then p1.y = p1.y + p1.speed*dt; movP1 = true end
+
+
+        if love.keyboard.isDown("right") and p2.x < limite_der then p2.x = p2.x + p2.speed*dt; p2.tira = 1; movP2 = true end
+        if love.keyboard.isDown("left") and p2.x > limite_izq then p2.x = p2.x - p2.speed*dt; p2.tira = 2; movP2 = true end
+        if love.keyboard.isDown("up") and p2.y > limite_sup then p2.y = p2.y - p2.speed*dt; movP2 = true end
+        if love.keyboard.isDown("down") and p2.y < limite_inf then p2.y = p2.y + p2.speed*dt; movP2 = true end
+
+        if checkCollision(p1, oro) then p1.score = p1.score + 1; respawnOro(limite_izq, limite_der, limite_sup, limite_inf) end
+        if checkCollision(p2, oro) then p2.score = p2.score + 1; respawnOro(limite_izq, limite_der, limite_sup, limite_inf) end
+    end
+
+    p1_anim:update(dt, movP1, p1.tira)
+    p2_anim:update(dt, movP2, p2.tira)
+    oro_anim:update(dt, true, 1)
 end
 
-function checkCollision(p, o)
-    return p.x < o.x + o.w and p.x + p.w > o.x and p.y < o.y + o.h and p.y + p.h > o.y
+function checkCollision(a, b)
+    return a.x < b.x + b.w and a.x + a.w > b.x and a.y < b.y + b.h and a.y + a.h > b.y
 end
 
-function respawnOro()
-    oro.x = math.random(50, 750)
-    oro.y = math.random(50, 550)
+function respawnOro(minX, maxX, minY, maxY)
+    oro.x = math.random(minX, maxX)
+    oro.y = math.random(minY, maxY)
 end
 
 function love.draw()
-    love.graphics.setColor(1, 1, 1) -- Reset de color
+
+    love.graphics.clear(0, 0, 0)
+
+    love.graphics.setColor(1, 1, 1) 
+
+
     if imgFondo then
         love.graphics.draw(imgFondo, 0, 0, 0, 800/imgFondo:getWidth(), 600/imgFondo:getHeight())
-    else
-        love.graphics.clear(0.2, 0.3, 0.2) -- Verde oscuro si falla
     end
 
-    if imgP1 then
-        love.graphics.draw(imgP1, p1.x, p1.y)
-    else
-        love.graphics.setColor(0, 0.4, 1)
-        love.graphics.rectangle("fill", p1.x, p1.y, p1.w, p1.h)
-    end
+    love.graphics.setShader(shaderTransparencia)
+        p1_anim:draw(p1.x, p1.y)
+        p2_anim:draw(p2.x, p2.y)
+        oro_anim:draw(oro.x, oro.y)
+    love.graphics.setShader()
 
-    love.graphics.setColor(1, 1, 1)
-    if imgP2 then
-        love.graphics.draw(imgP2, p2.x, p2.y)
-    else
-        love.graphics.setColor(1, 0.2, 0.2)
-        love.graphics.rectangle("fill", p2.x, p2.y, p2.w, p2.h)
-    end
-
-    love.graphics.setColor(1, 1, 1)
-    if imgOro then
-        love.graphics.draw(imgOro, oro.x, oro.y)
-    else
-        love.graphics.setColor(1, 0.9, 0)
-        love.graphics.circle("fill", oro.x + 16, oro.y + 16, 16)
-    end
-
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.print("P1: " .. p1.score, 30, 20, 0, 1.5, 1.5)
-    love.graphics.print("P2: " .. p2.score, 700, 20, 0, 1.5, 1.5)
-    love.graphics.printf("TIEMPO: " .. math.ceil(gameTimer), 0, 20, 800, "center", 0, 1.5, 1.5)
+    love.graphics.print("P1: "..p1.score, 20, 20, 0, 1.5)
+    love.graphics.print("P2: "..p2.score, 720, 20, 0, 1.5)
+    love.graphics.printf("TIEMPO: "..math.ceil(gameTimer), 0, 20, 800, "center", 0, 1.5)
 
     if isGameOver then
-        love.graphics.setColor(0, 0, 0, 0.8)
+        love.graphics.setColor(0, 0, 0, 0.7)
         love.graphics.rectangle("fill", 0, 0, 800, 600)
         love.graphics.setColor(1, 1, 1)
-        love.graphics.printf("FIN DEL JUEGO\nPresiona 'R' para reiniciar", 0, 280, 800, "center", 0, 2, 2)
+        love.graphics.printf("¡TIEMPO AGOTADO!\n'R' Reiniciar | 'M' Sonido", 0, 280, 800, "center", 0, 2)
     end
 end
 
-function love.keypressed(key)
-    if key == "r" then love.event.quit("restart") end
+function love.keypressed(k)
+    if k == "r" then love.event.quit("restart") end
+    if k == "m" then
+        sonidoActivo = not sonidoActivo
+        love.audio.setVolume(sonidoActivo and 1 or 0)
+    end
 end
